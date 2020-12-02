6bd0a449faae0a60b284121a756e543fad495987
JoelKanyi <joelkanyi98@gmail.com>
2020-12-02 21:42:17 +0300

    trash code, but working

--- app/src/main/java/com/kanyideveloper/lovecalculator/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/lovecalculator/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.kanyideveloper.lovecalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+
+        val service = retrofit.create(RestAPI::class.java)
+        val call : Call<Results> = service.getLoversResult("Joel","Beatrice")
+
+        call.enqueue(object : Callback<Results>{
+            override fun onFailure(call: Call<Results>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<Results>, response: Response<Results>) {
+                Toast.makeText(this@MainActivity,response.body().toString(),Toast.LENGTH_LONG).show()
+            }
+
+        })
 
     }
 }
--- app/src/main/java/com/kanyideveloper/lovecalculator/RestAPI.kt
+++ app/src/main/java/com/kanyideveloper/lovecalculator/RestAPI.kt
@@ -1,5 +1,16 @@
 package com.kanyideveloper.lovecalculator
 
+import retrofit2.Call
+import retrofit2.http.*
+
+
+const val BASE_URL = "https://love-calculator.p.rapidapi.com/"
+
 interface RestAPI {
 
+    @Headers("x-rapidapi-key: 72ca1bbcd4msh2635007a217ffb8p140999jsndca5f4311295")
+    @GET("getPercentage")
+    fun getLoversResult(@Query("fname") fName: String, @Query("sname") sName: String) : Call<Results>
 }
+
+//"x-rapidapi-key: 72ca1bbcd4msh2635007a217ffb8p140999jsndca5f4311295", "x-rapidapi-host: love-calculator.p.rapidapi.com"
