028223ab4f111b660b367f9e6278b564d4a2025c
JoelKanyi <joelkanyi98@gmail.com>
2020-12-02 18:45:09 +0300

    added the arch and retrofit dependencies

---
+++ app/src/main/java/com/kanyideveloper/lovecalculator/RestAPI.kt
@@ -0,0 +1,5 @@
+package com.kanyideveloper.lovecalculator
+
+interface RestAPI {
+
+}
---
+++ app/src/main/java/com/kanyideveloper/lovecalculator/Results.kt
@@ -0,0 +1,3 @@
+package com.kanyideveloper.lovecalculator
+
+data class Results(val fname: String = "", val sname: String = "", val percentage: Int = 0, val result: String = "")
